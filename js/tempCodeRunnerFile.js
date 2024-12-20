abd7beec132bed962bbde532a0f7b60f9d8a55fd
Anas Anwar <anasanwar210@gmail.com>
2024-12-20 16:14:59 +0200

    Structure Is Done

---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+//   const inputs = document.querySelectorAll("input");
+//   const button = document.querySelector("button");
+//   inputs.forEach((input) => {
+//     input.addEventListener("input", () => {
+//       button.disabled = false;
+//     });
+//   });
+// });
