ab88cb40f0b80bb1934fa451271ce472cda0e6e1
Anas Anwar <anasanwar210@gmail.com>
2024-12-21 19:08:29 +0200

    Finish App

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const formEle = document.querySelector("form");
 const inputEle = document.querySelector("input");
+const loading = document.querySelector(".loading");
 
 const apiKey = "676581a560a208ee1fde7e30";
 
@@ -9,7 +10,9 @@ getAllTodos();
 
 formEle.addEventListener("submit", (e) => {
   e.preventDefault();
-  addTodos();
+  if (inputEle.value.trim().length > 0) {
+    addTodos();
+  }
 });
 
 async function addTodos() {
@@ -26,22 +29,29 @@ async function addTodos() {
     body: JSON.stringify(todoText),
   };
 
-  const response = await fetch(
-    "https://todos.routemisr.com/api/v1/todos",
-    option
-  );
+  try {
+    loading.classList.remove("d-none");
+    const response = await fetch(
+      "https://todos.routemisr.com/api/v1/todos",
+      option
+    );
 
-  if (response.ok) {
-    const data = await response.json();
-    if (data.message === "success") {
-      console.log(inputEle.value);
-      await getAllTodos();
-      formEle.reset();
+    if (response.ok) {
+      const data = await response.json();
+      if (data.message === "success") {
+        toastr.success("Have fun storming the castle!", "Miracle Max Says");
+        await getAllTodos();
+        formEle.reset();
+      }
     }
+    loading.classList.add("d-none");
+  } catch (error) {
+    console.log(error);
   }
 }
 
 async function getAllTodos() {
+  loading.classList.remove("d-none");
   const response = await fetch(
     `https://todos.routemisr.com/api/v1/todos/${apiKey}`
   );
@@ -51,63 +61,213 @@ async function getAllTodos() {
     if (data.message === "success") {
       console.log(data);
       displayTodos();
+      checkTaskIsEmpty();
+      completedTodos();
     }
   }
+  loading.classList.add("d-none");
 }
 
+// function displayTodos() {
+//   let todoList = ``;
+//   for (let i = 0; i < allTodos.length; i++) {
+//     todoList += `
+//       <li class="d-flex justify-content-between align-items-center col-12 py-2 border-bottom">
+//         <span
+//         style="${allTodos[i].completed ? "text-decoration: line-through;" : ""}"
+//         class="taskName"
+//         onclick="markCompleted('${allTodos[i]._id}')"
+//         data-tooltip="Click to complete">
+//         ${i + 1}- ${allTodos[i].title}
+//         </span>
+//         <div class="d-flex align-items-center gap-3">
+//           ${
+//             allTodos[i].completed
+//               ? `<span>
+//               <i class="fa-regular fa-circle-check" style="color: #63e6be;"></i>
+//             </span>`
+//               : ""
+//           }
+//           <span onclick="deleteTodo('${allTodos[i]._id}')"
+//           class="icon"><i class="fa-solid fa-trash-can"></i></span>
+//         </div>
+//       </li>
+//     `;
+//   }
+//   document.getElementById("taskData").innerHTML = todoList;
+// }
+
 function displayTodos() {
-  let todoList = ``;
-  for (let i = 0; i < allTodos.length; i++) {
-    todoList += `
-      <li class="d-flex justify-content-between align-items-center col-12 py-2 border-bottom">
-        <span 
-        style="${allTodos[i].completed ? "text-decoration: line-through;" : ""}"
-        class="taskName" data-tooltip="Click to complete">
-        ${i + 1}- ${allTodos[i].title}
-        </span>
-        <div class="d-flex align-items-center gap-3">
-          ${
-            allTodos[i].completed
-              ? `
-              <span>
-                <i
-                  class="fa-regular fa-circle-check"
-                  style="color: #63e6be;"
-                ></i>
-              </span>`
-              : ""
-          }
-          <span onclick="deleteTodo('${
-            allTodos[i]._id
-          }')" class="icon"><i class="fa-solid fa-trash-can"></i></span>
-        </div>
-      </li>
-    `;
-  }
-  document.getElementById("taskData").innerHTML = todoList;
+  const taskData = document.getElementById("taskData");
+  taskData.innerHTML = "";
+  allTodos.forEach((todo, index) => {
+    // Start Li
+    const li = document.createElement("li");
+    li.className =
+      "d-flex justify-content-between align-items-center col-12 py-2 border-bottom";
+
+    // Start taskNameSpan
+    let span = document.createElement("span");
+    span.style.textDecoration = todo.completed ? "line-through" : "";
+    span.className = "taskName";
+    span.dataset.todoId = todo._id;
+    span.setAttribute("data-tooltip", "Click to complete");
+    span.innerText = `${index + 1}- ${todo.title}`;
+    if (todo.completed) {
+      removeClickEvent(span);
+    } else {
+      span.addEventListener("click", markCompletedHandler);
+    }
+
+    li.appendChild(span);
+
+    // Start iconsDiv
+    const div = document.createElement("div");
+    div.className = "d-flex align-items-center gap-3";
+
+    // Start checkSpan
+    const checkSpan = document.createElement("span");
+    checkSpan.className = todo.completed ? "completed" : "";
+    checkSpan.innerHTML = todo.completed
+      ? `<i class="fa-regular fa-circle-check" style="color: #63e6be;"></i>`
+      : "";
+    div.appendChild(checkSpan);
+
+    // Start deleteSpan
+    const deleteSpan = document.createElement("span");
+    deleteSpan.className = "icon";
+    deleteSpan.addEventListener("click", () => deleteTodo(todo._id));
+    deleteSpan.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    div.appendChild(deleteSpan);
+
+    // Append Div to Li
+    li.appendChild(div);
+
+    // Append Li to TaskData
+    taskData.appendChild(li);
+  });
 }
 
 async function deleteTodo(id) {
-  console.log(id);
-  const todoId = {
-    todoId: id,
-  };
-  const option = {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: "btn btn-success",
+      cancelButton: "btn btn-danger",
     },
-    body: JSON.stringify(todoId),
-  };
+    buttonsStyling: false,
+  });
+  swalWithBootstrapButtons
+    .fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "No, cancel!",
+      reverseButtons: false,
+    })
+    .then(async (result) => {
+      if (result.isConfirmed) {
+        loading.classList.remove("d-none");
+        const todoId = {
+          todoId: id,
+        };
+        const option = {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(todoId),
+        };
 
-  const response = await fetch(
-    "https://todos.routemisr.com/api/v1/todos",
-    option
-  );
-  if (response.ok) {
-    const data = await response.json();
-    if (data.message === "success") {
-      getAllTodos();
+        const response = await fetch(
+          "https://todos.routemisr.com/api/v1/todos",
+          option
+        );
+        if (response.ok) {
+          const data = await response.json();
+          if (data.message === "success") {
+            swalWithBootstrapButtons.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+            getAllTodos();
+          }
+        }
+        checkTaskIsEmpty();
+        loading.classList.add("d-none");
+      }
+    });
+}
+
+function markCompletedHandler(event) {
+  const id = event.target.dataset.todoId;
+  markCompleted(id);
+}
+
+async function markCompleted(id) {
+  Swal.fire({
+    title: "You Want To Complete this?",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, Complete it!",
+  }).then(async (result) => {
+    if (result.isConfirmed) {
+      loading.classList.remove("d-none");
+      const todoId = {
+        todoId: id,
+      };
+      const option = {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(todoId),
+      };
+      const response = await fetch(
+        "https://todos.routemisr.com/api/v1/todos",
+        option
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        if (data.message === "success") {
+          Swal.fire({
+            title: "Your task has been Completed.",
+            icon: "success",
+          });
+          getAllTodos();
+        }
+      }
+      loading.classList.add("d-none");
     }
+  });
+}
+
+function checkTaskIsEmpty() {
+  const taskList = document.getElementById("taskData");
+  if (!allTodos.length) {
+    taskList.classList.add("d-none");
+  } else {
+    taskList.classList.remove("d-none");
   }
+}
+
+function completedTodos() {
+  const allTasks = document.querySelector(".allTasks");
+  const completed = document.querySelector(".completed");
+  const completedTask = allTodos.filter((todo) => todo.completed);
+  allTasks.innerHTML = allTodos.length;
+  completed.innerHTML = completedTask.length;
+  document.getElementById("progress").style.width = `${
+    (completedTask.length / allTodos.length) * 100
+  }%`;
+}
+
+function removeClickEvent(spanElement) {
+  spanElement.removeEventListener("click", markCompletedHandler);
+  spanElement.style.cursor = "not-allowed";
+  spanElement.dataset.tooltip = "Task completed";
 }
--- js/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-//   const inputs = document.querySelectorAll("input");
-//   const button = document.querySelector("button");
-//   inputs.forEach((input) => {
-//     input.addEventListener("input", () => {
-//       button.disabled = false;
-//     });
-//   });
-// });
